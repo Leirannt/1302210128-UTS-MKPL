103771ada212555b3ca1fb6fadd1f4ca86ed26a2
Leirannt <93717185+Leirannt@users.noreply.github.com>
2024-04-21 21:57:13 +0700

    memperbaiki bad smell
    memperbaiki long parameter dan  conditional complexity pada kodingan

--- TaxCalculator/src/lib/TaxFunction.java
+++ TaxCalculator/src/lib/TaxFunction.java
@@ -20,32 +20,25 @@ public class TaxFunction {
 	 */
 
 	// long parameter list & conditional complexity
+	private static final int NON_TAXABLE_INCOME = 54000000;
+	private static final int MARRIED_ADDITIONAL = 4500000;
+	private static final int CHILD_ADDITIONAL = 1500000;
+
 	public static int calculateTax(int monthlySalary, int otherMonthlyIncome, int numberOfMonthWorking, int deductible,
 			boolean isMarried, int numberOfChildren) {
 
-		int tax = 0;
-
 		if (numberOfMonthWorking > 12) {
-			System.err.println("More than 12 month working per year");
+			System.err.println("More than 12 months working per year");
 		}
 
-		if (numberOfChildren > 3) {
-			numberOfChildren = 3;
-		}
+		numberOfChildren = Math.min(numberOfChildren, 3);
 
-		if (isMarried) {
-			tax = (int) Math.round(0.05 * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible
-					- (54000000 + 4500000 + (numberOfChildren * 1500000))));
-		} else {
-			tax = (int) Math.round(
-					0.05 * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible - 54000000));
-		}
+		int nonTaxableIncome = NON_TAXABLE_INCOME + (isMarried ? MARRIED_ADDITIONAL : 0)
+				+ (numberOfChildren * CHILD_ADDITIONAL);
 
-		if (tax < 0) {
-			return 0;
-		} else {
-			return tax;
-		}
+		int taxableIncome = (monthlySalary + otherMonthlyIncome) * numberOfMonthWorking - deductible - nonTaxableIncome;
+
+		return Math.max((int) Math.round(0.05 * taxableIncome), 0);
 	}
 
 }
